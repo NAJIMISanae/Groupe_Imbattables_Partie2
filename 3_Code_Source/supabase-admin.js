b94257a04e6abe2bb6d1fe67fcbfc4d21d28c8d0
NAJIMISanae <nasanae@etudiant-esic.fr>
2026-01-22 17:57:13 +0100

    Delete 3_Code_Source directory

--- 3_Code_Source/supabase-admin.js
+++
@@ -1,8 +0,0 @@
-// supabase-admin.js
-import 'dotenv/config';
-import { createClient } from '@supabase/supabase-js';
-
-export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
